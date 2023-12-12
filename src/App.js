a71a3f6a64201d2cea84ff26289d7658cc012226
dz-vadim <46889822+dz-vadim@users.noreply.github.com>
2023-12-13 01:43:11 +0200

    Added posts and post lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css"
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: "Title text 1", body: "Description"},
+    {id: 2, title: "Title text 2", body: "Description"},
+    {id: 3, title: "Title text 3", body: "Description"},
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostList posts={posts} title="firlst list"/>
+      <PostList posts={posts} title="second list"/>
     </div>
   );
 }
